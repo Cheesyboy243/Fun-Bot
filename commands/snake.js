42fe0e5aee00421f1d5e187f4235e88a23c234c7
Cheesyboy243 <elkinsjames255@gmail.com>
2024-11-14 16:09:01 -0600

    Create snake.js

---
+++ commands/snake.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { Snake } = require('discord-gamecord');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName(`snake`)
+    .setDescription(`Play a game of snake`),
+    async execute (interaction) {
+
+        const Game = new Snake({
+            message: interaction,
+            isSlashGame: true,
+            embed: {
+              title: 'Snake Game',
+              overTitle: 'Game Over',
+              color: '#5865F2'
+            },
+            emojis: {
+              board: '⬛',
+              food: '🍎',
+              up: '⬆️', 
+              down: '⬇️',
+              left: '⬅️',
+              right: '➡️',
+            },
+            stopButton: 'Stop',
+            timeoutTime: 60000,
+            snake: { head: '🟢', body: '🟩', tail: '🟢', over: '💀' },
+            foods: ['🍎', '🍆', '🍊', '🥕', '🥝', '🌽'],
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+          });
+          
+          Game.startGame();
+          Game.on('gameOver', result => {
+                return;
+          });
+
+
+
+
+    }}
